3e3590d1a05dc0a123c6901c56d86122ceefeaf8
AleksandrukV <valentynaleksandruk2005@gmail.com>
2024-03-17 14:54:44 +0200

    Add the code to the file Task_3

---
+++ Home-work_2/Task_3.cpp
@@ -0,0 +1,23 @@
+#include"Library.h"
+
+void CopyWithChangeOfCase(FILE* f, FILE* f2)
+{
+	char c = fgetc(f);
+	while (c != EOF)
+	{
+		if (c >= 'A' && c <= 'Z')
+			fprintf(f2, "%c", c + 32);
+		else 
+			fprintf(f2, "%c", c);
+		c = fgetc(f);
+	}
+}
+
+int main()
+{
+	FILE* f = fopen("d:\\temp\\file.txt", "r");
+	FILE* f2 = fopen("d:\\temp\\file2.txt", "w");
+	CopyWithChangeOfCase(f, f2);
+	fclose(f);
+	fclose(f2);
+}
